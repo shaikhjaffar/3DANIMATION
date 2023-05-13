93cbef33920465e319c2978e91d88b7139200088
Mohammed Jaffar SHAIKH <92439144+shaikhjaffar@users.noreply.github.com>
2023-05-13 18:25:28 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,39 @@
+
+
+
+(function(){
+  function id(v){ return document.getElementById(v); }
+  function loadbar() {
+    var ovrl = id("overlay"),
+        prog = id("progress"),
+        stat = id("progstat"),
+        img = document.images,
+        c = 0,
+        tot = img.length;
+    if(tot == 0) return doneLoading();
+
+    function imgLoaded(){
+      c += 1;
+      var perc = ((100/tot*c) << 0) +"%";
+      prog.style.width = perc;
+      stat.innerHTML = "Loading "+ perc;
+      if(c===tot) return doneLoading();
+    }
+    function doneLoading(){
+      setTimeout(function(){ 
+        ovrl.style.opacity = 0;
+        ovrl.style.display = "none";
+      }, 1200);
+    }
+    for(var i=0; i<tot; i++) {
+      var tImg     = new Image();
+      tImg.onload  = imgLoaded;
+      tImg.onerror = imgLoaded;
+      tImg.src     = img[i].src;
+    }    
+  }
+  document.addEventListener('DOMContentLoaded', loadbar, false);
+}());
 function locomotive() {
   gsap.registerPlugin(ScrollTrigger);
 
